723cfc5ff0175fad64be81ef3d439421b817aa63
Ayin <2752655197@qq.com>
2024-05-25 21:49:22 +0800

    feat: 删除添加自定义地图模块

--- app/map_selection.py
+++ app/map_selection.py
@@ -65,32 +65,6 @@ class MapSelection:
             y = 155 + (i - 1) // 2 * 155
             self.screen.blit(self.default_maps_preview[i - 1], (164 + ((i - 1) % 2) * 150, 35 + (i - 1) // 2 * 155))
             self.screen.blit(name, (x, y))
-    
-    # 自定义地图包
-    def display_diy_pack(self):
-        tips = [
-            "ESC: 强制返回",
-            "Enter: 保存",
-            "P: 说明"
-        ]
-        color = (255, 255, 255)
-        pygame.draw.rect(self.screen, color, (164, 25, 120, 120), 1)
-        for index, it in enumerate(tips):
-            text = self.small_font.render(it, True, color)
-            self.screen.blit(text, (224 - text.get_width() // 2, 50 + index * 25))
-        if self.map_selected == 1:
-            color = (255, 255, 0)
-        text = self.small_font.render("创建地图", True, color)
-        self.screen.blit(text, (192, 150))
-        for i in range(2, self.map_num + 2):
-            if i == self.map_selected:
-                color = (255, 255, 0)
-            else:
-                color = (255, 255, 255)
-            name = self.small_font.render(self.diy_maps[i - 2], True, color)
-            x = 350 - (i % 2) * 150
-            y = 130 + (i - 1) // 2 * 150
-            self.screen.blit(name, (x, y))
 
     def display(self):
         # 清空屏幕
@@ -112,21 +86,12 @@ class MapSelection:
                 color = (0, 255, 255)
         else:
             color = (255, 255, 255)
-        diy_pack = self.font.render("自定义", True, color)
-        if self.pack_selected == 2:
-            color = (255, 255, 0)
-        else:
-            color = (255, 255, 255)
         back_opt = self.font.render("返回", True, color)
         self.screen.blit(default_pack, (30, 30))
-        self.screen.blit(diy_pack, (28, 70))
         self.screen.blit(back_opt, (30, 110))
 
         # 地图列表
-        if self.pack_selected == 0:
-            self.display_default_pack()
-        if self.pack_selected == 1:
-            self.display_diy_pack()
+        self.display_default_pack()
 
         pygame.display.flip()
     
@@ -136,28 +101,18 @@ class MapSelection:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP or event.key == pygame.K_w:  # 上
                 if self.column == 0:  # 切换地图包
-                    self.pack_selected = (self.pack_selected + 2) % 3
-                    if self.pack_selected == 0:
-                        self.map_num = DEFAULT_NUM
-                    else:
-                        self.map_num = self.diy_nums
+                    self.pack_selected = (self.pack_selected + 1) % 2
                 else:  # 切换地图
                     if self.map_selected - 2 > 0:
                         self.map_selected = self.map_selected - 2
             elif event.key == pygame.K_DOWN  or event.key == pygame.K_s:  # 下
                 if self.column == 0:  # 切换地图包
-                    self.pack_selected = (self.pack_selected + 1) % 3
-                    if self.pack_selected == 0:
-                        self.map_num = DEFAULT_NUM
-                    else:
-                        self.map_num = self.diy_nums
+                    self.pack_selected = (self.pack_selected + 1) % 2
                 else:  # 切换地图
                     if self.map_selected + 2 <= self.map_num:
                         self.map_selected = self.map_selected + 2
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:  # 左
                 if self.column == 1:
-                    if self.pack_selected == 2:
-                        return None
                     self.column = 0
                     self.map_selected = 0
                     self.pack_selecting = True
@@ -166,26 +121,20 @@ class MapSelection:
                     self.map_selected -= 1
             elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:  # 右
                 if self.column == 0:
-                    if self.pack_selected == 2:
-                        return None
                     self.column = 1
                     self.map_selected = 1
                     self.pack_selecting = False
                 elif self.column == 1:
-                    if self.pack_selected == 1 and self.diy_nums == 0:
-                        return None
                     self.column = 2
                     if self.map_selected + 1 <= self.map_num:
                         self.map_selected += 1
                     
             elif event.key == pygame.K_RETURN:
-                if self.pack_selected == 2:  # 返回
+                # 选择地图
+                if self.map_selected > 0:
                     self.pack_selected = 0
+                    return "开始游戏"
+                # 返回
+                if self.pack_selected == 1 and self.map_selected == 0:
                     return "主菜单"
-                if self.pack_selected == 1 or self.pack_selected == 0: # 选择地图
-                    if self.pack_selected == 1 and self.map_selected == 1:  # 创建地图
-                        return "创建地图"
-                    if self.map_selected > 0:
-                        self.pack_selected = 0
-                        return "开始游戏"
         return None
